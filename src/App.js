d4ff66eef6d7440770ffe686d3b5cbe8e6ce36d0
sreedevi-sree <sreedevifsd@gmail.com>
2021-12-27 17:48:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,67 @@
 const API = [
-  {heading:"450 x 300",
+  {
+    heading: "450 x 300",
     name: "Fancy Product",
     price: "40.00 - $80.00",
-    
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Special Item",
     price: "18.00",
-    
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Sale Item",
     price: "25.00",
-   
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Popular Item",
     price: "40.00",
-    
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Sale Item",
     price: "25.00",
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Fancy Product",
     price: "40.00 - $80.00",
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Special Item",
     price: 18.0,
+    count: 0,
+    counterVal: 1,
     inCart: false
   },
   {
-    heading:"450 x 300",
+    heading: "450 x 300",
     name: "Popular Item",
     price: "40.00",
+    count: 0,
+    counterVal: 1,
     inCart: false
   }
 ];
 export default API;
-
-
