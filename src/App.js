85d0d9d943ec63af48490302584de03b581be44e
sreedevi-sree <sreedevifsd@gmail.com>
2022-01-07 23:44:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,151 +1,57 @@
-import {useState} from "react";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import ToggleButton from '@mui/material/ToggleButton';
 
 import "./index.css";
-// import { style } from "@mui/system";
-export default function App() {
+import React, { Component } from "react";
 
-  const Initialmovie = [
-    {
-      name: "Thuppaki",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BZTc4YWY5MzAtOTc4Zi00NDVmLThlMGItYjliOGNkYmM3NDBmXkEyXkFqcGdeQXVyOTk3NTc2MzE@._V1_.jpg",
-      rating: "9",
-      year: "2012",
-      summary:
-"An army captain visits Mumbai to be with his family and find a suitable bride. However, an explosion in the city sets him off on a mission to find and disable a terrorist sleeper cell in the city."
-    },
-    {
-      name: "Spiderman",
-      poster:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQlKOtc-RTtE2ns8fMI990-RHrqGwncy_D0w&usqp=CAU",
-      rating: "7.13",
-      year: "2002",
-      summary:
-"Peter Parker's life changes when he is bitten by a genetically altered spider and gains superpowers. He uses his powers to help people and finds himself facing the Green Goblin, an evil maniac"
-    },
-    {
-      name: "Red notice",
-      poster:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlwYfCCXZcMjUGSM88PBwYhTvWEc_kjUu_Gw&usqp=CAU",
-      rating: " 6.4",
-      year: "2021",
-      summary:
-"In the world of international crime, an Interpol agent attempts to hunt down and capture the world's most wanted art thief."
-    },
-    {
-      name: "Hera pheri",
-      poster:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAeH5wJvCZbY6_lOec9vAmUNAQkBauGNViwA&usqp=CAU",
-      rating: "8.2",
-      year: "2000",
-      summary:
-"Two tenants and a landlord, in desperate need of money, chance upon a ransom call via a cross connection. They hatch a plan to claim the ransom for themselves"
-    },
-    {
-      name: "M S Dhoni",
-      poster:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ334LoTpdtqtYk17XPQjRcBim8qRyKiR1d6Q&usqp=CAU",
-      rating: "8",
-      year: "2016",
-      summary:
-"M S Dhoni, a boy from Ranchi, aspires to play cricket for India. Though he initially tries to please his father by working for the Indian Railways, he ultimately decides to chase his dreams"
-    }
-  ];
-  const [name,setName]=useState(" ");
-  const [poster,setPoster]=useState(" ");
-  const [rating,setRating]=useState(" ");
-  const [year,setYear]=useState(" ");
-  const [summary,setSummary]=useState(" ");
+class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
 
-const [movieList,setMovieList]=useState(Initialmovie)
-  return (
-    <div className="App">
-      <div className="MovieArray">
-        <h3>Enter Movie details</h3>
-        <TextField
-         value={name}
-         onChange={(event)=>
-           setName(event.target.value)}        
-        id="outlined-basic" label="Name" variant="outlined" />
-        <TextField       
-        value={poster}
-       onChange={(event)=>
-         setPoster(event.target.value)}
-        id="outlined-basic" label="Poster" variant="outlined" />
-        <TextField 
-        value={rating}
-      onChange={(event)=>
-        setRating(event.target.value)}
-        id="outlined-basic" label="Rating" variant="outlined" />
-        <TextField
-        value={year}
-      onChange={(event)=>
-        setYear(event.target.value)}
-         id="outlined-basic" label="Year" variant="outlined" />
-        <TextField
-        value={summary}
-      onChange={(event)=>
-        setSummary(event.target.value)}
-         id="outlined-basic" label="Summary" variant="outlined" />
-      
-</div>
-<Button 
-onClick={()=>{
-  const newMovie ={
-    name:name,
-    poster:poster,
-    rating:rating,
-    year:year,
-    summary:summary,
-
+    if (!e.target.email.value) {
+      alert("Email is required");
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    } else  {
+      alert("Successfully logged in");
+     
+    } 
   };
-  setMovieList([...movieList,newMovie])
-}}
-variant="outlined">Add Movie</Button>
 
-      <section className="movie-list">
-      {movieList.map((movie) => (
-        <Welcome
-          name={movie.name}
-          poster={movie.poster}
-          rating={movie.rating}
-          year={movie.year}
-          summary={movie.summary}
-        />
-      ))}
-      </section>
-    </div>
-  );
-}
-function Welcome({ name, poster, rating, year,summary }) {
-  const styles={color:rating>="8.5" ? "green": "red"};
-  const [showText, setShowText] = useState(false);
-  return (
-    <div className="new-movie">
-      <h1 className="movie-name">{name}</h1>
-      <img className="movie-poster" src={poster} alt={name} />{" "}
-      <p style={styles} className="movie-rating">Rating: ⭐{rating}/10</p>
-      <p className="movie-year">Year: {year}</p>
-      <ToggleButton
-  value="showText"
-  onClick={()=>setShowText(!showText)}>Toggle Description
-</ToggleButton>
-{showText && <p  className="movie-summary">{summary}</p>}      
-      <Counter />
+  handleClick = e => {
+    e.preventDefault();
+
+    alert("Go to registration page");
+  };
+
+  render() {
+    return (
+      <div className="App">
+            <div className="col-lg-12 col-md-6 col-sm-2" >
+            <h1>LOGIN PAGE</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgjM3_nvS_uvUPuovtzQJD_8nSFaZFWPmu7Q&usqp=CAU" alt=""></img>
+                
+            <form className="form" >
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Enter the email address" />
           </div>
-  );
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" placeholder="Enter the password" />
+          </div>
+          <button className="primary" onClick={this.handleSubmit}>LOGIN</button>
+          <button className="secondary" onClick={this.handleClick}>
+              SIGN UP
+        </button>
+        </form>
+            </div>
+            </div>   
+        
+        
+    );
+  }
 }
-function Counter ()
-{
-  const[like,setLike]=useState(0);
-  const[dislike,setDislike]=useState(0);
-  return (
-    <div className="counter">
-      <button onClick={()=>setLike(like+1)}> Like 👍 {like}</button>
-      <button onClick={()=>setDislike(dislike+1)}> disLike 👎 {dislike}</button>
-      </div>
-  ) ;
-}
+
+export default App;
